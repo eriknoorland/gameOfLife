c4eb00060f625a0ec461eff62f2dd30f05b2f9d8
Erik Noorland <noorlandjr_e@hotmail.com>
2024-02-11 18:40:12 +0100

    Update test descriptions

--- tests/getNextArrayIndex.test.ts
+++ tests/getNextArrayIndex.test.ts
@@ -1,11 +1,11 @@
 import getNextArrayIndex from '../src/getNextArrayIndex';
 
-describe('getNextArrayIndex tests', function() {
-  it('#getNextArrayIndex([1,2,3], 1) should return 2', function() {
+describe('getNextArrayIndex', function() {
+  it('should return the next array index when the given index is smaller than the total number of items - 1', function() {
     expect(getNextArrayIndex([1, 2, 3], 1)).toBe(2);
   });
 
-  it('#getNextArrayIndex([1,2,3], 2) should return 0', function() {
+  it('should return the first array index when the last array index is given', function() {
     expect(getNextArrayIndex([1, 2, 3], 2)).toBe(0);
   });
 });
--- tests/getPrevArrayIndex.test.ts
+++ tests/getPrevArrayIndex.test.ts
@@ -1,11 +1,11 @@
 import getPrevArrayIndex from '../src/getPrevArrayIndex';
 
-describe('getPrevArrayIndex tests', function() {
-  it('#getPrevArrayIndex([1,2,3], 1) should return 0', function() {
+describe('getPrevArrayIndex', function() {
+  it('should return the given index minus 1', function() {
     expect(getPrevArrayIndex([1, 2, 3], 1)).toBe(0);
   });
 
-  it('#getPrevArrayIndex([1,2,3], 0) should return 2', function() {
+  it('should return the last array index when the given index is 0', function() {
     expect(getPrevArrayIndex([1, 2, 3], 0)).toBe(2);
   });
 });
